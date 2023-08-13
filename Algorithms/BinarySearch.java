f8adb48983fa560a835c4793aacb84c65b74dbb7
Yugant1338 <122391669+Yugant1338@users.noreply.github.com>
2023-08-13 14:52:05 +0530

    Create BinarySearch.java
    Binary Search Algorithm

---
+++ Algorithms/BinarySearch.java
@@ -0,0 +1,15 @@
+   static int binarysearch(int[] arr,int start,int end,int key){
+       if(start<end){
+           int mid = start + (end-start)/2;
+           if(arr[mid]==key){
+               return mid;
+           }
+           else if(arr[mid]>key){
+               return binarysearch(arr,start,mid,key);
+           }
+           else{
+               return binarysearch(arr,mid+1,end,key);
+           }
+       }
+       return -1;
+   }
