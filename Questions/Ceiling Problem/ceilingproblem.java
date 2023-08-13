fa16c20e03d6306c1bf19e0a9ccce6744088eb20
Yugant1338 <122391669+Yugant1338@users.noreply.github.com>
2023-08-13 14:56:07 +0530

    Create ceilingproblem.java
    Find the index of the element in the array which is either equal or the least highest than the key.

---
+++ Questions/Ceiling Problem/ceilingproblem.java
@@ -0,0 +1,17 @@
+static int binarysearch(int[] arr,int start,int end,int key){
+        if(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]==key){
+                return mid;
+            }
+            else if(arr[mid]>key){
+                return binarysearch(arr,start,mid-1,key);
+            }
+            else{
+                return binarysearch(arr,mid+1,end,key);
+            }
+        }
+        else{
+            return arr[start];
+        }
+}
