7da0fd25ac0fc5afdec4d7e08bef87760e287183
Yugant1338 <122391669+Yugant1338@users.noreply.github.com>
2023-08-19 15:53:23 +0530

    Create LinearSort.java
    Linear Sorting Of An Array

---
+++ Algorithms/LinearSort.java
@@ -0,0 +1,15 @@
+static int[] linearSort(int[] arr) {
+	for (int i = 0; i < arr.length; i++) {
+    	for (int j = i + 1; j < arr.length; j++){
+			if (arr[i] > arr[j]) {	
+				swap(arr,i,j);
+      	 	}
+		}
+	}
+	return arr;
+}
+static void swap(int[] arr,int a,int b){
+	int temp = arr[a];
+    arr[a] = arr[b];
+	arr[b] = temp;
+}
