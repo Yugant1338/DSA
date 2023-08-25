f5766320fe856393bd83c5f96668894ad4e434b6
Yugant1338 <122391669+Yugant1338@users.noreply.github.com>
2023-08-25 20:14:23 +0530

    Create validprfctsqr.java
    Given a positive integer num, return true if num is a perfect square or false otherwise.
    
    A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
    
    You must not use any built-in library function, such as sqrt.

---
+++ Questions/Leet Code Problems/Easy/367. Valid Prefect Square/validprfctsqr.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int n = (int)Math.sqrt(num);
+        if(n*n==num){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
