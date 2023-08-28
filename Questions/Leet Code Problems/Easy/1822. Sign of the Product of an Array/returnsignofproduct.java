143b97155195d6e160996788f6ef17bec8105547
Yugant1338 <122391669+Yugant1338@users.noreply.github.com>
2023-08-28 23:40:57 +0530

    Create returnsignofproduct.java
    There is a function signFunc(x) that returns:
    
    1 if x is positive.
    -1 if x is negative.
    0 if x is equal to 0.
    You are given an integer array nums. Let product be the product of all values in the array nums.
    
    Return signFunc(product).

---
+++ Questions/Leet Code Problems/Easy/1822. Sign of the Product of an Array/returnsignofproduct.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int negative = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                return 0;
+            }
+            else if(nums[i]<0){
+                negative++;
+            }
+        }
+        if(negative%2==0){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+}
